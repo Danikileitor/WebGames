ce80a6fdf4cda5edd5bdc648324f4c30de31a9e5
Danikileitor <focusyi@hotmail.com>
2023-01-17 19:08:31 +0100

    RNGTD

--- web/js/RNGTD/main.js
+++ web/js/RNGTD/main.js
@@ -1,8 +1,14 @@
 const canvas = document.getElementById("RNGTD");
 const c = canvas.getContext("2d");
 
-canvas.width = 640;
-canvas.height = 384;
+canvas.width = 1280;
+canvas.height = 768;
 
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+const mapa1 = new Image();
+mapa1.onload = () => {
+  c.drawImage(mapa1, 0, 0, canvas.width, canvas.height);
+};
+mapa1.src = "assets/RNGTD/maps/mapa1.png";
