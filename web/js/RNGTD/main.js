98204c04f59f5a846bab4679207a49454c24638a
Danikileitor <focusyi@hotmail.com>
2023-01-17 18:45:46 +0100

    RNGTD

---
+++ web/js/RNGTD/main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("RNGTD");
+const c = canvas.getContext("2d");
+
+canvas.width = 640;
+canvas.height = 384;
+
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
