8dd971dd4f7b9fd84df8affaed747c9bdfb10127
Danikileitor <focusyi@hotmail.com>
2023-01-17 18:58:20 +0100

    RNGTD

--- web/assets/RNGTD/tilesets/bosque.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="bosque" tilewidth="16" tileheight="16" tilecount="625" columns="25">
- <image source="../../PixelCrawler/Environment/Green Woods/Assets/Tiles.png" width="400" height="400"/>
-</tileset>
